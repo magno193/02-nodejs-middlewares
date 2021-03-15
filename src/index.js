a6dd7d56a811a34e17ade1eb82ea9faa21d0d5b9
Alexandre Ferreira <alexmlf93@gmail.com>
2021-03-15 12:37:56 -0300

    Tests finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,53 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  if (user.pro || user.todos.length <= 10) {
+    next();
+  }
+  return response.status(403).json({ error: `Users with free tier are limited to 10 todos` });
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'The id is not valid' });
+  }
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+  request.user = user;
+  request.todo = todo;
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'The id is not valid' });
+  }
+  const user = users.find(user => user.id === id);
+  if (!user) {
+    return response.status(404).json({ error: 'User not found' });
+  }
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
